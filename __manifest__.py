b3244fc6e97a511a580984819d72bb62fcf5374d
dikewarda <dikeayuw@gmail.com>
2023-02-20 14:38:56 +0700

    Jobsheet 1 ERP

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    "nama"              : "New Addons",
+    "version"           : "1.0",
+    "author"            : "Dike Ayu Wardani",
+    "website"           : "https://www.detik.com/",
+    "category"          : "New Module",
+    "summary"           : "Ini adalah addons baru",
+    "description"       : """
+        Latihan membuat addons baru di odoo 1 
+    """,
+    "depends"           : [
+        "base",
+    ],
+    "data"              : [
+
+    ],
+    "demo"              : [],
+    "test"              : [],
+    "image"             : [],
+    "qweb"              : [],
+    "css"               : [],
+    "application"       : True,
+    "installable"       : True,
+    "auto_install"      : False,
+
+}
